e628300eda0cf720144fc9ec55c2c286b1065045
Nattapon Nimakul <totizy@gmail.com>
2011-04-12 17:16:55 +0700

    test

--- Blognone12-NewTheme/secondView.m
+++ Blognone12-NewTheme/secondView.m
@@ -4,7 +4,7 @@
 //
 //  Created by Nattapon on 3/21/11.
 //  Copyright 2011 iphone developer. All rights reserved.
-// change something
+// change something2
 
 #import "secondView.h"
 #import <QuartzCore/QuartzCore.h>
